66338ca733022423fec5d84aa7040da6451ed445
José Ignacio Carretero <joseignacio.carretero@gmail.com>
2020-06-22 12:31:58 +0200

    Added the capability to add /dev/kmsg character device to the nodes as a service

--- KubernetesOnLxcAnsible/InstallKubernetes.sh
+++ KubernetesOnLxcAnsible/InstallKubernetes.sh
@@ -12,3 +12,13 @@ ansible-playbook  -i container_inventory playbooks/InstallDockers.yaml
 ansible-playbook  -i container_inventory playbooks/ConfigureKubernetes.yaml
 
 # I can run the last 2 playbooks the param " -e host=10.0.3.7" to add new worker nodes.
+
+# Optionally, install a service to set up the /dev/kmsg character device.
+ansible-playbook  -i container_inventory playbooks/SetServiceMKNOD.yaml
+
+# Stop the containers:
+# ansible-playbook  -i inventory playbooks/CreateLxcContainers.yaml -e state=stopped
+
+# Restart the containers:
+# ansible-playbook  -i inventory playbooks/CreateLxcContainers.yaml -e state=started
+
