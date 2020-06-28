913b0708eee854ea8bf3364d6c8f630ef70026f8
José Ignacio Carretero <joseignacio.carretero@gmail.com>
2020-06-28 18:34:18 +0200

    Added some changes in the installation .yaml files and added a lot of comments to files

--- KollaAnsibleOnLXC/InstallContainer.sh
+++ KollaAnsibleOnLXC/InstallContainer.sh
@@ -1,8 +1,11 @@
+# Destroy every previos LXC Container here
 ansible-playbook  -i inventory playbooks/CreateLxcContainers.yaml -e state=absent
 
-
-for a in 172.23.32.2 172.23.32.5 172.23.32.6 172.23.32.7; do ssh-keygen -f /home/jicg/.ssh/known_hosts -R $a ; done
-for a in 172.23.16.2 172.23.16.5 172.23.16.6 172.23.16.7; do ssh-keygen -f /home/jicg/.ssh/known_hosts -R $a ; done
+# Create the containers again.
 ansible-playbook  -i inventory playbooks/CreateLxcContainers.yaml
+
+# Start the containers
 ansible-playbook  -i containers_inventory playbooks/PrepareContainers.yaml
 
+# To simply stop the containers use: 
+# ansible-playbook  -i inventory playbooks/CreateLxcContainers.yaml -e state=stopped
